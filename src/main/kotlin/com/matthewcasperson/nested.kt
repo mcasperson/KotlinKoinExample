cd69537727ae05c78d32ad633e917efa5f6c9ba9
Matthew Casperson <matthewcasperson@gmail.com>
2021-07-15 17:36:58 +1000

    Added nested example and uberjar build

---
+++ src/main/kotlin/com/matthewcasperson/nested.kt
@@ -0,0 +1,27 @@
+package com.matthewcasperson
+
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.dsl.module
+
+data class HelloMessageData(val message : String = "Hello from wrapped class!")
+
+interface HelloServiceWrapper {
+    fun hello(): String
+}
+
+class HelloServiceWrapperImpl(private val helloMessageData:HelloMessageData) : HelloServiceWrapper {
+    override fun hello() = helloMessageData.message
+}
+
+fun main() {
+    val helloService = module {
+        single { HelloMessageData() }
+        single { HelloServiceWrapperImpl(get()) as HelloServiceWrapper }
+    }
+
+    var app = startKoin {
+        modules(helloService)
+    }
+
+    println(app.koin.get<HelloServiceWrapper>().hello())
+}
